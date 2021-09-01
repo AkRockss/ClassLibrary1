589b7af84b3740e34a80e6557dbcdd213ab028c6
Aleksander Kruse Schaumburg-Müller <alek1082@edu.easj.dk>
2021-09-01 11:40:55 +0200

    Add project files.

---
+++ ClassLibrary1/Student.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Student
+    {
+
+        public string _name;
+        public string _adress;
+        public int _semester;
+
+
+
+        public string Name 
+        {
+            get => _name;
+
+            set
+            {
+                if (value.Length < 2) throw new ArgumentException("Name is smaller then 2");
+                _name = value;
+            }
+}
+                
+        public string Adress 
+        { 
+            get => _adress; 
+            
+            set
+            {
+                if (value == null) throw new ArgumentException("Adress value is null");
+                _adress = value;
+            }
+        
+        }
+        public int Semester 
+        {
+            get => _semester;
+            
+        set
+         {
+                if (1 <= value || value <= 8) _semester = value;
+                else throw new ArgumentOutOfRangeException("semester", value, "illegal Semester");
+
+         }
+       
+        }
+
+
+        public enum GenderType { Male, Female }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+
+        public Student (string Name, string Adress, int Semester)
+        {
+
+        }
+
+
+    }
+
+
+    class Teacher
+    {
+
+
+    }
+
+}
---
+++ ClassLibrary1Tests/StudentTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1.Tests
+{
+    [TestClass()]
+    public class StudentTests
+    {
+        [TestMethod()]
+        public void StudentNameTest()
+        {
+            Student student = new Student("Anders", "ValpeVej 2", 3);
+            student.Name = "Anders";
+            Assert.AreEqual("Anders", student.Name);
+
+            Assert.ThrowsException<ArgumentException>(
+                () => student.Name = "");
+
+          
+                   
+        }
+
+        [TestMethod()]
+        public void StudentAdressTest()
+        {
+            Student student = new Student("Anders", "ValpeVej 2", 3);
+            student.Adress = null;
+            Assert.IsNotNull(null, student.Adress);
+
+            Assert.ThrowsException<ArgumentException>(
+                () => student.Name = "");
+
+        }
+
+
+        [TestMethod()]
+        public void StudentSemesterTest()
+        {
+            Student student = new Student("Anders", "ValpeVej 2", 3);
+            student.Semester = -1;
+            Assert.AreEqual(-1, student.Semester);
+
+            Assert.ThrowsException<ArgumentException>(
+                () => student.Name = "");
+
+        }
+
+
+
+    }
+    }
